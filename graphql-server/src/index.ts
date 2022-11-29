54f30cd8e5892ca67cb517a8b3709f085bd41dbd
josejfml <jfml2014@gmail.com>
2022-11-29 19:13:45 +0100

    Aplicando env-cmd en server

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -10,7 +10,7 @@ import { createServer } from "node:http";
 import { createYoga } from 'graphql-yoga'
 const yoga = createYoga({ schema })
 // 2
-const port = Number(process.env.API_PORT) || 4000
+const port = Number(process.env.GRAPHQL_API_PORT) || 4000
 // 3
 const server = createServer( yoga );
 // 4
